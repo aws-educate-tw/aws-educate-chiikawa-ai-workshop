341cdf7813ad3f5617213054aa2037e69b2f834f
cdxvy30 <cdxvy30@gmail.com>
2025-04-01 23:36:58 +0800

    chore: remove useless code

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -13,10 +13,10 @@ from linebot.v3.messaging import (
     ReplyMessageRequest,
 )
 from linebot.v3.webhooks import (
-  MessageEvent,
-  TextMessageContent,
-   StickerMessageContent, # 新增的代碼
-  FollowEvent
+    MessageEvent,
+    TextMessageContent,
+    StickerMessageContent, # 新增的代碼
+    FollowEvent
 )
 import urllib.request
 
@@ -28,9 +28,6 @@ logger.setLevel(logging.INFO)
 configuration = Configuration(access_token=os.getenv('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
-# 移除兩個單獨的處理器
-# @handler.add(MessageEvent, message=TextMessageContent) 
-# @handler.add(MessageEvent, message=StickerMessageContent)
 
 # 使用單一通用處理器
 @handler.add(MessageEvent)
@@ -77,77 +74,6 @@ def handle_message(event):
                 messages=response
             )
         )
-# @handler.add(MessageEvent, message=TextMessageContent)
-# def handle_message(event):
-#     user_id = event.source.user_id
-#     user_message = event.message.text
-    
-#     profile_url = f'https://api.line.me/v2/bot/profile/{user_id}'
-#     headers = {
-#         'Authorization': f'Bearer {os.getenv("CHANNEL_ACCESS_TOKEN")}'
-#     }
-    
-#     req = urllib.request.Request(profile_url, headers=headers)
-    
-#     logger.info(f"發送請求獲取用戶資料: {profile_url}")
-#     with urllib.request.urlopen(req) as response:
-#         profile_json = json.loads(response.read().decode('utf-8'))
-    
-#     logger.info(f"獲取到用戶資料: {profile_json}")
-#     user_name = profile_json.get('displayName', '朋友')
-    
-#     logger.info(f"Received message from {user_id}: {user_message}")
-    
-#     response = run(user_id, user_name, user_message)
-
-#     logger.info(f"Response: {response}")
-    
-#     with ApiClient(configuration) as api_client:
-#         line_bot_api = MessagingApi(api_client)
-        
-#         line_bot_api.reply_message_with_http_info(
-#             ReplyMessageRequest(
-#                 reply_token=event.reply_token,
-#                 messages=response
-#             )
-#         )
-
-# # 新增的代碼
-# @handler.add(MessageEvent, message=StickerMessageContent)
-# def handle_sticker(event):
-#     user_id = event.source.user_id
-#     sticker_id = event.message.sticker_id
-#     package_id = event.message.package_id
-#     user_message = f"[STICKER] package_id={package_id}, sticker_id={sticker_id}"
-#     profile_url = f'https://api.line.me/v2/bot/profile/{user_id}'
-#     headers = {
-#         'Authorization': f'Bearer {os.getenv("CHANNEL_ACCESS_TOKEN")}'
-#     }
-    
-#     req = urllib.request.Request(profile_url, headers=headers)
-    
-#     logger.info(f"發送請求獲取用戶資料: {profile_url}")
-#     with urllib.request.urlopen(req) as response:
-#         profile_json = json.loads(response.read().decode('utf-8'))
-    
-#     logger.info(f"獲取到用戶資料: {profile_json}")
-#     user_name = profile_json.get('displayName', '朋友')
-    
-#     logger.info(f"Received message from {user_id}: {user_message}")
-    
-#     response = run(user_id, user_name, user_message)
-
-#     logger.info(f"Response: {response}")
-    
-#     with ApiClient(configuration) as api_client:
-#         line_bot_api = MessagingApi(api_client)
-        
-#         line_bot_api.reply_message_with_http_info(
-#             ReplyMessageRequest(
-#                 reply_token=event.reply_token,
-#                 messages=response
-#             )
-#         )
 
 def lambda_handler(event, context):
     try: 
